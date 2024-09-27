1eb92bd1244222a1bb23ad3bb13e31e390edc4aa
syahmimscs <syahmimcbs600@gmail.com>
2024-09-27 10:42:07 +0000

    Latest changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,4 +10,8 @@ export default defineConfig({
   define: {
     'process.env': process.env,
   },
+server: {
+    host: '0.0.0.0', // Allow access from outside the container
+    port: 5173,      // Ensure port matches the one being exposed
+  },
 });
